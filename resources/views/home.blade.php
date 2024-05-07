ffd96c2657fcb2e00e7d64bca11e6f4164bbbd93
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-05-07 18:15:19 +0200

    add jumbotron image and comic list section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,29 @@
 
 </section>
 
-{{--@foreach($comics as $comic)
+<section id="comic-list">
+    <div class="container">
+        <h2 class="text-center">COMIC LIST</h2>
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            @foreach($comics as $comic)
+            <div class="col">
+                <div class="card">
 
-        <ul>
-            @foreach($comic as $comicDetails)
-            <li>{{$comicDetails}}</li>
-@endforeach
-</ul>
 
-@endforeach--}}
+                    <img class="card-img-top" src="{{$comic['thumb']}}" alt="">
+
+
+                    <div class="card-body my-3">
+                        <h4>{{$comic['title']}}</h4>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+    </div>
+</section>
+
+
 
 @endsection
